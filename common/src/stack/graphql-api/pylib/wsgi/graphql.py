dd2ca0c7664d9163e47abce1154c217a4d8d2df2
Chris Ladd <chris.ladd@teradata.com>
2019-09-17 15:17:40 -0700

    Put the graphql endpoint behind JWT

--- common/src/stack/graphql-api/pylib/wsgi/graphql.py
+++ common/src/stack/graphql-api/pylib/wsgi/graphql.py
@@ -1,10 +1,15 @@
+from pathlib import Path
+
 from ariadne import QueryType, graphql_sync, gql, make_executable_schema
 from ariadne.constants import PLAYGROUND_HTML
 from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, jwt_required
 
 
 app = Flask(__name__)
-
+app.config["JWT_SECRET_KEY"] = Path("/opt/stack/etc/jwt.secret").read_text()
+app.config["JWT_ERROR_MESSAGE_KEY"] = "error"
+jwt = JWTManager(app)
 
 type_defs = gql("""
     type Query {
@@ -27,6 +32,7 @@ def graphql_playground():
 
 
 @app.route("/", methods=["POST"])
+@jwt_required
 def graphql_endpoint():
     success, result = graphql_sync(
         schema,
