a2cd8ee53ad53040d8f2d40dcba639d7817d0c8f
Chris Ladd <chris.ladd@teradata.com>
2019-09-17 15:53:35 -0700

    Start of a custom GraphQL Playground so we can handle auth

--- common/src/stack/graphql-api/pylib/wsgi/graphql/__init__.py
+++ common/src/stack/graphql-api/pylib/wsgi/graphql/__init__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from ariadne import QueryType, graphql_sync, gql, make_executable_schema
 from ariadne.constants import PLAYGROUND_HTML
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, request
 from flask_jwt_extended import JWTManager, jwt_required
 
 
@@ -12,33 +12,33 @@ app.config["JWT_ERROR_MESSAGE_KEY"] = "error"
 jwt = JWTManager(app)
 
 type_defs = gql("""
-    type Query {
-	hello: String!
-    }
+	type Query {
+		hello: String!
+	}
 """)
 
 query = QueryType()
 
 @query.field("hello")
 def resolve_hello(*_):
-    return "Hello!"
+	return "Hello!"
 
 schema = make_executable_schema(type_defs, query)
 
 
 @app.route("/", methods=["GET"])
 def graphql_playground():
-    return PLAYGROUND_HTML, 200
+	return render_template("index.html", token="TODO"), 200
 
 
 @app.route("/", methods=["POST"])
 @jwt_required
 def graphql_endpoint():
-    success, result = graphql_sync(
-        schema,
-        request.get_json(),
-        context_value=request,
-        debug=app.debug
-    )
+	success, result = graphql_sync(
+		schema,
+		request.get_json(),
+		context_value=request,
+		debug=app.debug
+	)
 
-    return jsonify(result), 200 if success else 400
+	return jsonify(result), 200 if success else 400
